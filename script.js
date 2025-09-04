b48905fae910aeab02d10f0f0d4b1e218189f01b
yanzada00 <ramos.yan@escola.pr.gov.br>
2025-09-04 09:05:18 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca=document.querySelectorA11('Btn-proximo');
+const reiniciarBtn=document.getElementById('Btn-reiniciar');
+
+avanca.forEach(button => {
+button.addEventListener('click',function(){   
+const atual = document.querySelector('.ativo');
+const proximopasso='passo'+this.getAttribute('data-proximo');
+} 
+
+);
+    
